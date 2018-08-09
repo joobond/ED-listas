bf1082d7f108f21d07ded39ded5d3170951f0c54
João Bond <jobond46@gmail.com>
2018-08-08 20:19:23 -0400

    Inserir no fim e em qualquer lugar

--- src/Lista.java
+++ src/Lista.java
@@ -1,10 +1,12 @@
 public class Lista {
 
     private No inicio;
+    private int quantidade;
 
     //Criando a lista
     public Lista(){
         this.inicio = null;
+        this.quantidade = 0;
     }
 
     //Verificando se a lista está vazia
@@ -14,6 +16,7 @@ public class Lista {
 
     //Se a lista estiver vazia, aponte para o inicio o nó que está sendo passado
     //Senão, aponte o nó que está passando para o inicio e troque o inicio para o nó que está passando
+    //É importante primeiro apontar para o inicial para depois trocar, senão perdemos a referência;
     public void inserirNoInicio(No no){
         if(isVazia()){
             this.inicio = no;
@@ -21,6 +24,7 @@ public class Lista {
             no.setProximo(this.inicio);
             this.inicio = no;
         }
+        this.quantidade++;
     }
 
     //Imprimindo a lista
@@ -34,5 +38,40 @@ public class Lista {
                 aux = aux.getProximo();
             }
         }
+    }
+
+    //Adicionando no fim da lista
+    public void inserirNoFim(No no){
+        if(isVazia()){
+            this.inicio = no;
+        }else{
+            No aux = this.inicio;
+            while(aux.getProximo() != null){
+                aux = aux.getProximo();
+            }
+            aux.setProximo(no);
+        }
+        this.quantidade++;
+    }
+
+    //Inserir em qualquer posição da lista
+    public void inserir(No no, int posicao){
+        if(isVazia() || posicao <= 1){
+            this.inserirNoInicio(no);
+        }else if(posicao > quantidade){
+            this.inserirNoFim(no);
+        }else{
+            No aux = this.inicio;
+            No ant = null;
+
+            for (int i = 1; i < posicao; i++){
+                ant = aux;
+                aux = aux.getProximo();
+            }
+
+            no.setProximo(ant.getProximo());
+            ant.setProximo(no);
+        }
+        this.quantidade++;
     }
 }
--- src/TestarLista.java
+++ src/TestarLista.java
@@ -3,9 +3,14 @@ public class TestarLista {
     public static void main(String[] args) {
         Lista lista = new Lista();
 
-        lista.inserirNoInicio(new No(45));
+        /*lista.inserirNoInicio(new No(45));
         lista.inserirNoInicio(new No(30));
-        lista.inserirNoInicio(new No(28));
+        lista.inserirNoInicio(new No(28));*/
+
+        lista.inserirNoFim(new No(40));
+        lista.inserirNoFim(new No(15));
+        lista.inserir(new No(2), 2);
+        lista.inserir(new No(3), 2);
 
         lista.imprimir();
     }
